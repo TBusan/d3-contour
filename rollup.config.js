91ba93c2496d0f58a0a9fa5c9c2629af1623570e
whitebluehorse <42024435+TBusan@users.noreply.github.com>
2025-10-28 12:34:33 +0800

    修改配置文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import {readFileSync} from "fs";
 import {terser} from "rollup-plugin-terser";
-import meta from "./package.json" assert {type: "json"};
+import meta from "./package.json" with {type: "json"};
 
 // Extract copyrights from the LICENSE.
 const copyright = readFileSync("./LICENSE", "utf-8")
